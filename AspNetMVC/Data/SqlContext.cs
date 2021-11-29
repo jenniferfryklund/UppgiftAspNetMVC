16468106d4c293af327dc97f568b2b82c83e7475
jenniferfryklund <Jennifer_fryklund.com>
2021-11-29 13:18:53 +0100

    Add project files.

---
+++ AspNetMVC/Data/SqlContext.cs
@@ -0,0 +1,18 @@
+﻿using AspNetMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
---
+++ AspNetMVC/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int SubCategoryId { get; set; }
+        
+        public virtual SubCategory SubCategory { get; set; }
+    }
+}
